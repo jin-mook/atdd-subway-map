30f25b66e784d3675efb4c57d7c4de9afe91bf88
Jin-Mook <jink0608@naver.com>
2024-07-04 15:04:19 +0900

    [step1] 지하철 역 삭제 인수 테스트 작성

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -3,6 +3,7 @@ package subway;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -96,6 +97,48 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("특정 지하철 역을 삭제하면 해당 지하철역은 조회되지 않습니다.")
+    @Test
+    void deleteStations() {
+        // given
+        Map<String, String> body1 = Map.of("name", "station1");
+        Map<String, String> body2 = Map.of("name", "station2");
 
+        long stationId = RestAssured
+                .given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body1)
+                .when()
+                .post("/stations")
+                .then().extract().jsonPath().getLong("id");
+
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body2)
+                .when()
+                .post("/stations");
+
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .pathParam("id", stationId)
+                .when()
+                .delete("/stations/{id}");
+
+        // when
+        List<String> result = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+
+        // then
+        Assertions.assertThat(result).hasSize(1);
+        Assertions.assertThat(result).containsExactly("station2");
+    }
 }
